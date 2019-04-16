2f3f628d7ccc03e64bf18b3044ba6f79d099020c
Carl Clark <carl.clark@dealersolutions.com.au>
2019-04-16 11:51:18 +1000

    Introduced AddAuthHeader
    This adds the user's credentials onto all requests as a HTTP Authorization header rather than within the body of the request.

--- source/StopWatch/Jira/JiraApiRequestFactory.cs
+++ source/StopWatch/Jira/JiraApiRequestFactory.cs
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,153 +18,166 @@ using System;
 
 namespace StopWatch
 {
-    internal class AuthenticateNotYetCalledException : Exception
-    {
-    }
+	internal class AuthenticateNotYetCalledException : Exception
+	{
+	}
 
-    internal class JiraApiRequestFactory : IJiraApiRequestFactory
-    {
-        #region public methods
-        public JiraApiRequestFactory(IRestRequestFactory restRequestFactory)
-        {
-            this.restRequestFactory = restRequestFactory;
-            this.username = "";
-            this.password = "";
-        }
-
-
-        public IRestRequest CreateValidateSessionRequest()
-        {
-            var request = restRequestFactory.Create("/rest/auth/1/session", Method.GET);
-            return request;
-        }
+	internal class JiraApiRequestFactory : IJiraApiRequestFactory
+	{
+		#region public methods
+		public JiraApiRequestFactory(IRestRequestFactory restRequestFactory)
+		{
+			this.restRequestFactory = restRequestFactory;
+			this.username = "";
+			this.password = "";
+		}
 
 
-        public IRestRequest CreateGetFavoriteFiltersRequest()
-        {
-            var request = restRequestFactory.Create("/rest/api/2/filter/favourite", Method.GET);
-            return request;
-        }
-        
-
-        public IRestRequest CreateGetIssuesByJQLRequest(string jql)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/search?jql={0}&maxResults=200", jql), Method.GET);
-            return request;
-        }
+		public IRestRequest CreateValidateSessionRequest()
+		{
+			var request = restRequestFactory.Create("/rest/auth/1/session", Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
 
 
-        public IRestRequest CreateGetIssueSummaryRequest(string key)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}", key.Trim()), Method.GET);
-            return request;
-        }
+		public IRestRequest CreateGetFavoriteFiltersRequest()
+		{
+			var request = restRequestFactory.Create("/rest/api/2/filter/favourite", Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
+		
 
-        public IRestRequest CreateGetIssueTimetrackingRequest(string key)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}?fields=timetracking", key.Trim()), Method.GET);
-            return request;
-        }
-
-
-        public IRestRequest CreatePostWorklogRequest(string key, DateTimeOffset started, TimeSpan time, string comment, EstimateUpdateMethods adjustmentMethod, string adjustmentValue)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/worklog", key.Trim()), Method.POST);
-            request.RequestFormat = DataFormat.Json;
-            request.AddBody(new
-                {
-                    timeSpent = JiraTimeHelpers.TimeSpanToJiraTime(time),
-                    started = JiraTimeHelpers.DateTimeToJiraDateTime(started),
-                    comment = comment
-                }
-            );
-            switch(adjustmentMethod) {
-                case EstimateUpdateMethods.Leave:
-                    request.AddQueryParameter("adjustEstimate", "leave");
-                    break;
-                case EstimateUpdateMethods.SetTo:
-                    request.AddQueryParameter("adjustEstimate", "new");
-                    request.AddQueryParameter("newEstimate", adjustmentValue);
-                    break;
-                case EstimateUpdateMethods.ManualDecrease:
-                    request.AddQueryParameter("adjustEstimate", "manual");
-                    request.AddQueryParameter("reduceBy", adjustmentValue);
-                    break;
-                case EstimateUpdateMethods.Auto:
-                    request.AddQueryParameter("adjustEstimate", "auto");
-                    break;
-            }
-            return request;
-        }
-
-        public IRestRequest CreateGetConfigurationRequest()
-        {
-            return restRequestFactory.Create("/rest/api/2/configuration", Method.GET);
-        }
+		public IRestRequest CreateGetIssuesByJQLRequest(string jql)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/search?jql={0}&maxResults=200", jql), Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
 
 
-        public IRestRequest CreatePostCommentRequest(string key, string comment)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/comment", key.Trim()), Method.POST);
-            request.RequestFormat = DataFormat.Json;
-            request.AddBody(new
-                {
-                    body = comment
-                }
-            );
-            return request;
-        }
+		public IRestRequest CreateGetIssueSummaryRequest(string key)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}", key.Trim()), Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
 
-        public IRestRequest CreateGetAvailableTransitions(string key)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/transitions", key.Trim()), Method.GET);
-            return request;
-        }
-
-        public IRestRequest CreateDoTransition(string key, int transitionId)
-        {
-            var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/transitions", key.Trim()), Method.POST);
-            request.RequestFormat = DataFormat.Json;
-            request.AddBody(new
-                {
-                    transition = new
-                    {
-                        id = transitionId
-                    }
-                }
-            );
-            return request;
-        }
-
-        public IRestRequest CreateAuthenticateRequest(string username, string password)
-        {
-            this.username = username;
-            this.password = password;
-
-            var request = restRequestFactory.Create("/rest/auth/1/session", Method.POST);
-            request.RequestFormat = DataFormat.Json;
-            request.AddBody(new {
-                username = this.username,
-                password = this.password
-            });
-            return request;
-        }
-
-        public IRestRequest CreateReAuthenticateRequest()
-        {
-            if (string.IsNullOrEmpty(this.username))
-                throw new AuthenticateNotYetCalledException();
-
-            return CreateAuthenticateRequest(this.username, this.password);
-        }
-        #endregion
+		public IRestRequest CreateGetIssueTimetrackingRequest(string key)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}?fields=timetracking", key.Trim()), Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
 
 
-        #region private members
-        private IRestRequestFactory restRequestFactory;
+		public IRestRequest CreatePostWorklogRequest(string key, DateTimeOffset started, TimeSpan time, string comment, EstimateUpdateMethods adjustmentMethod, string adjustmentValue)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/worklog", key.Trim()), Method.POST);
+			request.RequestFormat = DataFormat.Json;
+			request.AddBody(new
+				{
+					timeSpent = JiraTimeHelpers.TimeSpanToJiraTime(time),
+					started = JiraTimeHelpers.DateTimeToJiraDateTime(started),
+					comment = comment
+				}
+			);
+			switch(adjustmentMethod) {
+				case EstimateUpdateMethods.Leave:
+					request.AddQueryParameter("adjustEstimate", "leave");
+					break;
+				case EstimateUpdateMethods.SetTo:
+					request.AddQueryParameter("adjustEstimate", "new");
+					request.AddQueryParameter("newEstimate", adjustmentValue);
+					break;
+				case EstimateUpdateMethods.ManualDecrease:
+					request.AddQueryParameter("adjustEstimate", "manual");
+					request.AddQueryParameter("reduceBy", adjustmentValue);
+					break;
+				case EstimateUpdateMethods.Auto:
+					request.AddQueryParameter("adjustEstimate", "auto");
+					break;
+			}
+			AddAuthHeader(request);
+			return request;
+		}
 
-        private string username;
-        private string password;
-        #endregion
-    }
+		public IRestRequest CreateGetConfigurationRequest()
+		{
+			var request = restRequestFactory.Create("/rest/api/2/configuration", Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
+
+
+		public IRestRequest CreatePostCommentRequest(string key, string comment)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/comment", key.Trim()), Method.POST);
+			request.RequestFormat = DataFormat.Json;
+			request.AddBody(new
+				{
+					body = comment
+				}
+			);
+			AddAuthHeader(request);
+			return request;
+		}
+
+		public IRestRequest CreateGetAvailableTransitions(string key)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/transitions", key.Trim()), Method.GET);
+			AddAuthHeader(request);
+			return request;
+		}
+
+		public IRestRequest CreateDoTransition(string key, int transitionId)
+		{
+			var request = restRequestFactory.Create(String.Format("/rest/api/2/issue/{0}/transitions", key.Trim()), Method.POST);
+			request.RequestFormat = DataFormat.Json;
+			request.AddBody(new
+				{
+					transition = new
+					{
+						id = transitionId
+					}
+				}
+			);
+			AddAuthHeader(request);
+			return request;
+		}
+
+		public IRestRequest CreateAuthenticateRequest(string username, string password)
+		{
+			this.username = username;
+			this.password = password;
+
+			var request = restRequestFactory.Create("/rest/auth/1/session", Method.GET);
+			request.RequestFormat = DataFormat.Json;
+			AddAuthHeader(request);
+			return request;
+		}
+
+		private void AddAuthHeader(IRestRequest request)
+		{
+			request.AddHeader("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{this.username}:{this.password}")));
+		}
+
+		public IRestRequest CreateReAuthenticateRequest()
+		{
+			if (string.IsNullOrEmpty(this.username))
+				throw new AuthenticateNotYetCalledException();
+
+			return CreateAuthenticateRequest(this.username, this.password);
+		}
+		#endregion
+
+
+		#region private members
+		private IRestRequestFactory restRequestFactory;
+
+		private string username;
+		private string password;
+		#endregion
+	}
 }
